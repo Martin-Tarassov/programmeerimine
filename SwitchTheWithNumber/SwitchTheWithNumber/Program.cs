e65cd961f8f4ddf1045554cb1340d63df57736d7
Martin Tarassov <epicgamesmega1@gmail.com>
2026-03-01 00:48:51 +0200

    Add files via upload

---
+++ SwitchTheWithNumber/SwitchTheWithNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SwitchTheWithNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Teie töö on teha switch rakendus
+           //kus on kolm case (1,2,3)
+           //Iga case puhul teeb erinev arv beep-e
+            Console.WriteLine("Sisesta number 1, 2 või 3");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1"
+                    Console.Beep(1000,500);
+                    Console.Beep(1000, 500);
+                    Console.WriteLine("Üks beep tehtud.");
+                    break;
+                    
+                case "2":
+                    Console.Beep(1000,500);
+                    Console.Beep(1000, 500);
+                    Console.Beep(1000, 500);
+                    Console.WriteLine("Kaks beep tehtud");
+                    break;
+                    
+                case "3":
+                    Console.Beep(1000, 500);
+                    Console.Beep(1000, 500);
+                    Console.Beep(1000, 500);
+                    Console.Beep(1000, 500);
+                    Console.WriteLine("Kolm beep tehtud");
+                    break;
+                    
+                default:
+                    Console.WriteLine("Valve number");
+                    break ;
+
+            }
+        }
+    }
+}
